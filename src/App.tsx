7da80ddf98df97b75ea9b46d146887742fdb9866
진민욱 <uk@jinmin-ug-ui-MacBookPro.local>
2022-04-09 16:17:04 +0900

    Add: init webpack

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+interface Props {}
+
+const App = ({}: Props) => {
+  return <div>App!!!!!ddddd</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
